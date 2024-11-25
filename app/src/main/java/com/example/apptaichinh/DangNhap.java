40c95e6507b61a21d3c56fcf46ed321d57988a79
vuongquyen05 <prohvl432@gmail.com>
2024-11-25 23:59:38 +0700

    quyền

--- app/src/main/java/com/example/apptaichinh/DangNhap.java
+++ app/src/main/java/com/example/apptaichinh/DangNhap.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DangNhap extends AppCompatActivity {
 
+    EditText et_sdt, et_mk;
     Button btn_dangnhap, btn_dangky;
 
     @Override
