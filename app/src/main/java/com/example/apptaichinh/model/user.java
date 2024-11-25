9edd99b185ad3553c42696ab6f3115fd154114b8
vuongquyen05 <prohvl432@gmail.com>
2024-11-25 23:48:34 +0700

    quyền

---
+++ app/src/main/java/com/example/apptaichinh/model/user.java
@@ -0,0 +1,37 @@
+package com.example.apptaichinh.model;
+
+public class user {
+    private String name;
+    private int Sdt;
+    private String mk;
+
+    public user(String name, int sdt, String mk) {
+        this.name = name;
+        Sdt = sdt;
+        this.mk = mk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSdt() {
+        return Sdt;
+    }
+
+    public void setSdt(int sdt) {
+        Sdt = sdt;
+    }
+
+    public String getMk() {
+        return mk;
+    }
+
+    public void setMk(String mk) {
+        this.mk = mk;
+    }
+}
