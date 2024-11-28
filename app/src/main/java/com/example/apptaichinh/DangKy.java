1f2c1fc9456430b5d1c9e6973c0c88b352032ac5
Admin <nguyenanhdung4953@gmail.com>
2024-11-28 10:17:52 +0700

    phamvandung

--- app/src/main/java/com/example/apptaichinh/DangKy.java
+++ app/src/main/java/com/example/apptaichinh/DangKy.java
@@ -2,8 +2,11 @@ package com.example.apptaichinh;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +16,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DangKy extends AppCompatActivity {
 
-    Button btn_dangky,btn_huy;
+    Button btn_dangky, btn_huy;
+    EditText et_user, et_sdt, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,22 +29,42 @@ public class DangKy extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        et_user = findViewById(R.id.etUsername);
+        et_sdt = findViewById(R.id.etPhoneNumberRegister);
+        et_password = findViewById(R.id.etPasswordRegister);
         btn_huy = findViewById(R.id.btnHuy);
         btn_dangky = findViewById(R.id.btnRegisterSubmit);
 
         btn_dangky.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(DangKy.this, DangNhap.class);
-                startActivity(intent);
-            }
-        });
+                String user = et_user.getText().toString().trim();
+                String sdt = et_sdt.getText().toString().trim();
+                String password = et_password.getText().toString().trim();
 
+                if (TextUtils.isEmpty(user)) {
+                    Toast.makeText(DangKy.this, "Vui lòng nhập tên người dùng!", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(sdt)) {
+                    Toast.makeText(DangKy.this, "Vui lòng nhập số điện thoại!", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(DangKy.this, "Vui lòng nhập mật khẩu!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(DangKy.this, "Đăng ký thành công!", Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(DangKy.this, DangNhap.class);
+                    startActivity(intent);
+
+                }
+            }
+
+
+        });
         btn_huy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(DangKy.this, DangNhap.class);
                 startActivity(intent);
+
             }
         });
     }
